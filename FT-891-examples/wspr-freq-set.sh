5d019edb3d103f266590be6c1bcc6f6dbf3a237a
Kenji Rikitake <kenji.rikitake@acm.org>
2020-12-29 12:57:49 +0000

    Update FT-891-examples

--- FT-891-examples/wspr-freq-set.sh
+++ FT-891-examples/wspr-freq-set.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # FT-891: DATA-U, 350Hz, 7.0386MHz, Pwr 5W, IPO (PREAMP 0), AGC Auto (see agc_level_e in rig.h)
-cd /home/kenji/wrk
 rigctl -m 1036 -r /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_00CED32F-if00-port0 -P RIG -s 38400 - << .EOF.
 M PKTUSB 350
 F 7038600
--- FT-891-examples/wspr-xmit-simple.sh
+++ FT-891-examples/wspr-xmit-simple.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-cd /home/kenji/wrk
+#
+# Running this script will send a prebuilt WAV file as a WSPR beacon
+# Set rig to PKTUSB mode
+# Note: use Hamlib 4.1 (git master branch) or later
+#
+# Turn on PTT: enable TX
 rigctl -m 1036 -r /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_00CED32F-if00-port0 -P RIG -s 38400 T 1 t
+# Send sound
 aplay -D hw:3,0 ./jj1bdx-wspr-1536hz.wav
+# Turn off PTT: disable TX
 rigctl -m 1036 -r /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_00CED32F-if00-port0 -P RIG -s 38400 T 0 t
