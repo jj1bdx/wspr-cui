7a7d104d654f45e2788af709b11d838845e37e8f
Kenji Rikitake <kenji.rikitake@acm.org>
2021-01-04 02:48:24 +0000

    Remove wspr-trx/wspr-xmit-simple.sh

--- wspr-trx/wspr-xmit-simple.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /home/kenji/wspr-trx
-# Turn PTT on by CAT
-rigctl -m 1036 -r /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_00CED32F-if00-port0 -P RIG -s 38400 T 1 t
-# Play back the recorded audio data for WSPR
-aplay -D hw:3,0 ./jj1bdx-wspr-1536hz.wav
-# Turn PTT off by CAT
-rigctl -m 1036 -r /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_00CED32F-if00-port0 -P RIG -s 38400 T 0 t
