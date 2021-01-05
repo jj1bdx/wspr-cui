f498cab18c4d0d8d8ce1ded173c58918217f8556
Kenji Rikitake <kenji.rikitake@acm.org>
2021-01-05 04:52:23 +0000

    wspr-trx-event-loop.py: update PTT switch, add event handler to disable transmission when interrupted

--- wspr-trx/wspr-trx-event-loop.py
+++ wspr-trx/wspr-trx-event-loop.py
@@ -6,14 +6,39 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 import random
 import serial
+import signal
+import sys
+import time
 
 # probability of transmission in percent times 2
 # (40 for 20% probability)
 txpercent = 40
-# PTT control port (FT-891 with SCU-17 here, assert RTS)
-pttport = "SCU_17_PTT_PORT(if01)"
 # Remembering transmission state of txrxjob()
 lasttx = False
+
+# PTT control port (SCU-17 connected to FT-891 here, assert RTS for PTT)
+pttport = "SCU_17_PTT_PORT(if01)"
+# Initialize PTT port as a global PySerial object
+pttobject = serial.serial_for_url(pttport, rtscts = False, dsrdtr = False, do_not_open=True)
+
+def pttobject_set_rts(value):
+    # You cannot reopen a port
+    # The port must be closed before it is reopened
+    if pttobject.is_open:
+      pttobject.close()
+    # Set RTS value
+    pttobject.rts = value
+    # Change to the control line state of a serial port
+    # requires open()ing the port
+    pttobject.open()
+
+def signal_handler(signal, frame):
+    print("Disable transmission", flush=True)
+    pttobject_set_rts(False)
+    print('Terminated by CTRL/C')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def txrxjob():
     # Required: otherwise lasttx is treated as a local variable
@@ -24,16 +49,15 @@ def txrxjob():
     x = random.randrange(0, 99)
     # Do not transmit continuously in multiple time slots
     if ((x < txpercent) and (lasttx == False)):
-      # Turn PTT On
-      s = serial.serial_for_url(pttport)
-      s.rts=False 
+      # Turn PTT On (but turn off first)
+      pttobject_set_rts(False)
       print("Enable transmission", flush=True)
-      s.rts=True
+      pttobject_set_rts(True)
       print("Start transmission", flush=True)
       # Play back audio
       subprocess.run(["aplay", "-q", "-D", "AUDIO_OUTPUT_DEVICE", "full path of the transmitted WAV file"])
       # Turn PTT Off
-      s.rts=False 
+      pttobject_set_rts(False)
       print("Complete transmission", flush=True)
       print("Transmission completed at:", end=" ", flush=True)
       subprocess.run(["date", "--rfc-3339=ns"])
