ea86a2505ae46a3d4d8935a000efea86ab156d66
Kenji Rikitake <kenji.rikitake@acm.org>
2022-12-28 00:49:22 +0000

    Add FT-710 and FT-891 examples

---
+++ FT-710-examples/freqset-6m.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd /home/kenji/wspr-trx
+# FT-710
+#  set frequency to 50.2930MHz
+#  set mode to DATA-U
+#  (not required: set 6m MAX output power to 5W / send_raw ; EX030402005;)
+#  set actual output power to 5W / send_raw ; PC005;
+#  set filter to narrow (300Hz) / send_raw ; NA01;
+# then check the status including PTT
+rigctl -m 1049 -r /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_011BD871-if00-port0 -P RIG -s 38400 - << .EOF.
+F 50293000
+M PKTUSB 2400
+send_raw ; PC005;
+send_raw ; NA01;
+f 
+m 
+send_raw ; PC;
+send_raw ; NA0;
+t
+.EOF.
---
+++ FT-891-examples/freqset-10m.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd /home/kenji/wspr-trx
+# FT-891, set mode to DATA-U and filter width 350Hz,
+# frequency to 14.0956MHz, output power to 5W, then check the status
+# (including PTT)
+rigctl -m 1036 -r /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_00CED32F-if00-port0 -P RIG -s 38400 - << .EOF.
+M PKTUSB 250 
+F 28124600
+L RFPOWER 0.05
+f 
+m 
+l RFPOWER
+t
+.EOF.
