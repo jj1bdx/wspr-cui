48c4d8fa3823a61cdaf6080996fb15c7afec6be6
Kenji Rikitake <kenji.rikitake@acm.org>
2023-05-25 11:21:17 +0900

    Add IC-705 freqset examples

---
+++ IC-705-examples/freqset-15m.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd /home/kenji/wspr-trx
+# IC-705
+#  set frequency to 21.0946MHz
+#  set mode to USB-D
+#  set filter to 300Hz
+#  set actual output power to 1W (10%)
+#  set preamp to disable (0)
+# then check the status including PTT
+rigctl -m 3085 -r /dev/serial/by-id/usb-Icom_Inc._IC-705_IC-705_31002769-if00 -P RIG -s 19200 - << .EOF.
+F 21094600
+M PKTUSB 300
+L RFPOWER 0.102
+L PREAMP 0
+f 
+m 
+l RFPOWER
+l PREAMP
+t
+.EOF.
